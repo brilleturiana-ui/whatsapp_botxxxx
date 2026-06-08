53572a092e5892f30945a5272c1ceaca9063c463
brilleturiana-ui <brilleturiana@gmail.com>
2026-06-08 19:08:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Wambugu Kinyua',
   botName: "Kinyua Bot",
   botOwner: 'Wambugu Kinyua', // Your name
-  ownerNumber: '2547xxxxxx', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '918280823289', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
